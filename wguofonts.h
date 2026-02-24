842902ec159abd6dac45862cc73f44d1395682fa
ctc999 <131505313+ctc999@users.noreply.github.com>
2026-02-24 11:11:58 +0800

    src/

---
+++ wguofonts.h
@@ -0,0 +1,10 @@
+//wguofonts 库
+//8x16 dot matrix characters (8x16 点阵字符)
+
+#pragma once
+#include <Adafruit_GFX.h>
+
+extern const GFXfont wguofonts;
+
+// 8x16 dot matrix characters
+// Approx. 1516 bytes
